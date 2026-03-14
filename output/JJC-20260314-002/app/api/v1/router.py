46e5fee561b0614bbd8fa4611046f0eca0683994
ZYQ <zyq838997125@gmail.com>
2026-03-14 19:00:25 +0800

    chore: remove output/ from git and update .gitignore
    - Add output/ to .gitignore
    
    - Remove user-generated output files from repo

--- output/JJC-20260314-002/app/api/v1/router.py
+++
@@ -1,9 +0,0 @@
-"""API v1 router configuration."""
-from fastapi import APIRouter
-from app.api.v1.endpoints import auth, users
-
-api_router = APIRouter()
-
-# Include endpoint routers
-api_router.include_router(auth.router)
-api_router.include_router(users.router)
